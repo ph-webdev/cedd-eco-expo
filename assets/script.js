5e2db2eb848e0899948058a692098556361408b0
peterhohk <peterhohk0409@gmail.com>
2023-09-28 12:59:22 +0800

    initial commit

---
+++ assets/script.js
@@ -0,0 +1,24 @@
+"use strict";
+
+(function() {
+
+
+// handle opening and closing of popouts
+
+const popoutShows = document.querySelectorAll(".popout-show");
+const popoutHides = document.querySelectorAll(".popout-hide");
+for (let ps of popoutShows) {
+  ps.addEventListener("click", () => {
+    const popout = document.querySelector(`.popout[data-pid="${ps.dataset.pid}"]`);
+    popout.showModal();
+  });
+}
+for (let ph of popoutHides) {
+  ph.addEventListener("click", () => {
+    const popout = ph.closest(".popout");
+    popout.close();
+  });
+}
+
+
+})();
