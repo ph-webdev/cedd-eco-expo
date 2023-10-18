1b03d284ef6333b428c9223a9acf7bf757bf058d
peterhohk <peterhohk0409@gmail.com>
2023-10-18 12:58:15 +0800

    added switching booth image by scrolling

--- assets/script.js
+++ assets/script.js
@@ -3,22 +3,41 @@
 (function() {
 
 
-// change booth image
+// change booth image on scroll and on drag of the scroll button
 
-const navButtons = document.querySelectorAll(".pc-nav button");
-const boothImgs = document.querySelectorAll(".booth-img");
-navButtons.forEach((button, buttonIndex) => {
-  button.addEventListener("click", () => {
-    const indexToShow = buttonIndex % boothImgs.length;
-    boothImgs.forEach((img, imgIndex) => {
-      if (imgIndex === indexToShow) {
-        img.classList.add("active");
-      } else {
-        img.classList.remove("active");
-      }
-    });
-  });
-});
+const boothImgs = document.querySelectorAll(".booth-img.outer");
+let rotationTracker = 0.5;
+function showBoothImg(indexToShow) {
+  boothImgs.forEach((img, imgIndex) => {
+    if (imgIndex === indexToShow) {
+      img.classList.add("active");
+    } else {
+      img.classList.remove("active");
+    }
+  })
+}
+
+window.addEventListener("wheel", (e) => {
+  rotationTracker += (e.deltaY > 0) ? 0.125 : (e.deltaY < 0) ? -0.125 : 0;
+  rotationTracker = (rotationTracker + boothImgs.length) % boothImgs.length;
+  showBoothImg(Math.floor(rotationTracker));
+})
+
+const scrollButton = document.querySelector(".scroll-button");
+function dragStart() {
+  window.addEventListener("mousemove", dragMove);
+  window.addEventListener("mouseup", dragEnd);
+}
+function dragMove(e) {
+  rotationTracker += e.movementY / 64;
+  rotationTracker = (rotationTracker + boothImgs.length) % boothImgs.length;
+  showBoothImg(Math.floor(rotationTracker));
+}
+function dragEnd() {
+  window.removeEventListener("mousemove", dragMove);
+  window.removeEventListener("mouseup", dragEnd);
+}
+scrollButton.addEventListener("mousedown", dragStart);
 
 // handle opening and closing of popouts
 
