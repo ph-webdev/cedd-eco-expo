40618f0e67c4066bc4555b642b8f923a129f0eaf
peterhohk <peterhohk0409@gmail.com>
2023-10-18 18:27:29 +0800

    added swiping function

--- assets/script.js
+++ assets/script.js
@@ -3,11 +3,13 @@
 (function() {
 
 
-// booth images handling setup
+// booth views handling setup
 
 const boothImgs = document.querySelectorAll(".booth-img.outer");
 let rotationTracker = 0.5;
-function showBoothImg(indexToShow) {
+function showBoothImg() {
+  rotationTracker = (rotationTracker + boothImgs.length) % boothImgs.length;
+  const indexToShow = Math.floor(rotationTracker);
   boothImgs.forEach((img, imgIndex) => {
     if (imgIndex === indexToShow) {
       img.classList.add("active");
@@ -17,27 +19,27 @@ function showBoothImg(indexToShow) {
   })
 }
 
-// change booth image by scrolling
+// change booth view by scrolling
 
+const scrollStepSize = document.body.offsetHeight / 4;
 window.addEventListener("wheel", (e) => {
-  rotationTracker += (e.deltaY > 0) ? 0.125 : (e.deltaY < 0) ? -0.125 : 0;
-  rotationTracker = (rotationTracker + boothImgs.length) % boothImgs.length;
-  showBoothImg(Math.floor(rotationTracker));
+  rotationTracker += e.deltaY / scrollStepSize;
+  showBoothImg();
 });
 
-// change booth image by dragging the scroll button
+// change booth view by dragging the scroll button
 
-let initialMouseY;
 const scrollButton = document.querySelector(".scroll-button");
+let initialMouseY;
+const dragStepSize = scrollButton.offsetWidth / 4;
 function dragStart(e) {
   window.addEventListener("mousemove", dragMove);
   window.addEventListener("mouseup", dragEnd);
   initialMouseY = e.clientY;
 }
 function dragMove(e) {
-  rotationTracker += e.movementY / 64;
-  rotationTracker = (rotationTracker + boothImgs.length) % boothImgs.length;
-  showBoothImg(Math.floor(rotationTracker));
+  rotationTracker += e.movementY / dragStepSize;
+  showBoothImg();
   if (e.clientY > initialMouseY) {
     scrollButton.classList.remove("scrolling-up");
     scrollButton.classList.add("scrolling-down");
@@ -55,9 +57,42 @@ function dragEnd() {
 }
 scrollButton.addEventListener("mousedown", dragStart);
 
-// change booth image by swipe
+// change booth view by swiping
 
-// WIP
+let activeTouchId = null;
+let lastTouchX;
+const swipeStepSize = document.body.offsetWidth / 4;
+function touchStart(e) {
+  if (activeTouchId !== null) return;
+  const touch = e.changedTouches.item(0);
+  activeTouchId = touch.identifier;
+  lastTouchX = touch.clientX;
+}
+function touchMove(e) {
+  if (activeTouchId === null) return;
+  for (let i = 0; i < e.changedTouches.length; i++) {
+    const touch = e.changedTouches.item(i);
+    if (touch.identifier === activeTouchId) {
+      rotationTracker += (touch.clientX - lastTouchX) / swipeStepSize;
+      showBoothImg();
+      lastTouchX = touch.clientX;
+    }
+  }
+}
+function touchEnd(e) {
+  if (activeTouchId === null) return;
+  for (let i = 0; i < e.changedTouches.length; i++) {
+    const touch = e.changedTouches.item(i);
+    if (touch.identifier === activeTouchId) {
+      activeTouchId = null;
+      break;
+    }
+  }
+}
+window.addEventListener("touchstart", touchStart);
+window.addEventListener("touchmove", (e) => { e.preventDefault(); touchMove(e); }, {passive: false});
+window.addEventListener("touchend", touchEnd);
+window.addEventListener("touchcancel", touchEnd);
 
 // handle opening and closing of popouts
 
